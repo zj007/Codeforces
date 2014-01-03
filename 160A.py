0dc530e7eff1b715ad7406124bf0f4b30088c7e3
zj007 <zjforit@163.com>
2014-01-03 02:58:39 -0800

    Type commit message here…

--- 160A.py
+++ 160A.py
@@ -1,3 +1,18 @@
 #python 2.7
 
-print 'hello'
+raw_input()
+coin_list = [int(e) for e in raw_input().split()]
+coin_list.sort(reverse = True)
+total_value = sum(coin_list)
+half_value = total_value / 2
+
+mine_value = 0
+
+for i, e in enumerate(coin_list):
+	mine_value += e
+	if mine_value > half_value:
+		break
+
+print i+1
+
+
